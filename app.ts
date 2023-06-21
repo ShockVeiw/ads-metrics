cffbda9954a21e3b4936079db68b879aeb010976
Leonid Fedorov <lfedorov887@gmail.com>
2023-06-21 19:24:12 +0300

    connect db

--- app.ts
+++ app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { AppDataSource } from './appDataSource';
+
 const app = express();
 const port = 8080;
 
@@ -6,6 +8,13 @@ app.get('/', (req, res) => {
     res.send("Hello world!");
 });
 
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${ port }`);
-});
+AppDataSource.initialize()
+    .then(() => {
+        console.log("data source has been initialized")
+        app.listen(port, () => {
+            console.log(`server started at http://localhost:${ port }`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    });
